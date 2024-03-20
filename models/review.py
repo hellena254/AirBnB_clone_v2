ea04501f3a643f881e735e8ce96a2838ea9ba33e
hellena254 <hellenah243@gmail.com>
2024-03-20 05:29:58 -0700

    review

--- models/review.py
+++ models/review.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+import os
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+
+class Review(BaseModel, Base):
+    """ The review class, contains text, place ID, and user ID """
+    __tablename__ = 'reviews'
+
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
