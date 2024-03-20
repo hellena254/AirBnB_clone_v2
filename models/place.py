2eda50e78cc0b457167d4c9ea2839605a3dcbd42
hellena254 <hellenah243@gmail.com>
2024-03-20 05:15:32 -0700

    place

--- models/place.py
+++ models/place.py
@@ -9,11 +9,11 @@ from models.review import Review
 from models.amenity import Amenity
 
 
-place_amenity = Table(
-    Base.metadata,
-    Column('place_id', String(60), ForeignKey('places.id'), nullable=False, primary_key=True),
-    Column('amenity_id', String(60), ForeignKey('amenities.id'), nullable=False, primary_key=True)
-)
+#place_amenity = Table(
+#    Base.metadata,
+#   Column('place_id', String(60), ForeignKey('places.id'), nullable=False, primary_key=True),
+#   Column('amenity_id', String(60), ForeignKey('amenities.id'), nullable=False, primary_key=True)
+#)
 
 
 """
@@ -35,34 +35,5 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    amenity_ids = []
+#    amenity_ids = []
     reviews = relationship('Review', cascade="all, delete, delete-orphan", backref='place'
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        amenities = relationship('Amenity', secondary=place_amenity, viewonly=False, backref='place_amenities')
-    else:
-        @property
-        def amenities(self):
-            """Returns the amenities"""
-            from models import storage
-            amenities_place = []
-            for value in storage.all(Amenity).values():
-                if value.id in self.amenity_ids:
-                    amenities_place.append(value)
-            return amenities_place
-
-        @amenities.setter
-        def amenities(self, value):
-            """Adds an amenity"""
-            if type(value) is Amenity:
-                if value.id not in self.amenity_ids:
-                    self.amenity_ids.append(value.id)
-
-        @property
-        def reviews(self):
-            """Returns the reviews"""
-            from models import storage
-            reviews_place = []
-            for value in storage.all(Review).values():
-                if value.place_id == self.id:
-                    reviews_place.append(value)
-            return reviews_place
