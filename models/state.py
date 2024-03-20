f95813c38a445cdab77a3dd948a3f4b36ad5f863
hellena254 <hellenah243@gmail.com>
2024-03-20 02:09:36 -0700

    db storage

--- models/state.py
+++ models/state.py
@@ -3,10 +3,27 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.city import City
+import os
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City"), cascade="all, delete", backref="state")
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship(
+            'City',
+            cascade='all, delete, delete-orphan',
+            backref='state'
+        )
+    else:
+        @property
+        def cities(self):
+            """Returns the cities in this State"""
+            from models import storage
+            cities_state = []
+            for v in storage.all(City).values():
+                if v.state_id == self.id:
+                    cities_state.append(v)
+            return cities_state
