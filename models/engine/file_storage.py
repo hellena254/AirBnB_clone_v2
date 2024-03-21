8f15a9879607f3792e6e594e908029cb82c829db
hellena254 <hellenah243@gmail.com>
2024-03-21 05:33:00 -0700

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,13 +2,7 @@
 """This module defines a class to manage file storage for hbnb clone"""
 import json
 import os
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
-from model.user import User
-from models.base_model import BaseModel
+from importlib import import_module
 
 
 class FileStorage:
