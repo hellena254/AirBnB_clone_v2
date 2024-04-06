603cac29d57763079e36faa19cb84fc8066b388b
hellena254 <hellenah243@gmail.com>
2024-04-05 23:50:13 -0700

    prepare your web server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,10 +5,29 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
+sudo ufw allow "Nginx HTTP"
 
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+# create the folders
+sudo mkdir -p /data/ 
+sudo mkdir -p /data/web_static/ 
+sudo mkdir -p /data/web_static/releases/ 
+sudo mkdir -p /data/web_static/releases/test 
+sudo mkdir -p /data/web_static/shared
+
+# fake html file
+sudo echo "<html>
+ <head></head>
+ <body>Testing my Configuration</body>
+ </html>" | sudo tee /data/web_static/releases/test/index.html
+
+# symbolic link liked to the below folder
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of the /data/ folder to the ubuntu user AND group
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+# Update the Nginx configuration to serve the content
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
+# restart for the effects to take place
 sudo service nginx start
