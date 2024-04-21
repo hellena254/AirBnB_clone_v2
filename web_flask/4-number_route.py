6ca2b31055b8fce44296c5b3b183c3ef9741cb52
hellena254 <hellenah243@gmail.com>
2024-04-21 07:26:53 -0700

    number only if integer

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -33,7 +33,7 @@ def python_text(text="is cool"):
     return "Python " + text.replace('_', ' ')
 
 
-@app.route('/number/<int:n>' strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def display_number(n):
     """display only if n is an integer"""
     return "{:d} is a number".format(n)
