18d9c9b47b3121a6591b1a6ff0e82ebde4c9c117
hellena254 <hellenah243@gmail.com>
2024-04-21 06:51:38 -0700

    c is fun

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application
+/: display “Hello HBNB!”
+/hbnb: display “HBNB”"""
+
+from flask import Flask
+# create a flask application
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ display Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """display HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun():
+    """display “C ” followed by the value of the text variable"""
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+if __name__ == "__main__":
+    """run the application"""
+    app.run(host='0.0.0.0', port=5000)
